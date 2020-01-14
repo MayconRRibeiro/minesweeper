bc9cd1d54b5de234cddc7a7302756fe9d78d7eb9
hoob3rt <pelczarskihubert@gmail.com>
2020-01-14 01:19:41 +0100

    moved css & images to appropriate directories

--- view/src/index.js
+++ view/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './css/App.css';
+import './css/Board.css';
+import './css/Cell.css';
+import './css/Game.css';
+import './css/Home.css';
+import './css/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
